a5dd97bfa5490e576143c5f36831d75dddba0f94
Rowan <79109185+rbarkerclarke@users.noreply.github.com>
2025-04-19 17:12:49 -0400

    Update WFutils.py

--- WFutils.py
+++ WFutils.py
@@ -9,25 +9,24 @@ try:
 except ImportError:
     import itertools
 
-pop = {'0':10}
 
 def flatten(listOfLists):
     "Flatten one level of nesting"
     return chain.from_iterable(listOfLists)
 
-
-def get_mutation_count():
-    mean = mutation_rate * pop_size * seq_length
+def get_mutation_count(pop, mu,N=sum(pop.values()),seq_length=1):
+    mean = mu * N * seq_length
     return np.random.poisson(mean)
 
-def get_random_haplotype():
+def get_random_haplotype(pop, generations=100):
+    pop_size=sum(pop.values())
     haplotypes = list(pop.keys())
     frequencies = [x/float(pop_size) for x in pop.values()]
     total = sum(frequencies)
     frequencies = [x / total for x in frequencies]
     return np.random.choice(haplotypes, p=frequencies)
 
-def get_mutant(haplotype):
+def get_mutant(pop, haplotype, seq_length=1, alphabet = ['0','1'], base_haplotype = "0"):
     site = np.random.randint(seq_length)
     possible_mutations = list(alphabet)
     possible_mutations.remove(haplotype[site])
@@ -36,63 +35,68 @@ def get_mutant(haplotype):
     return new_haplotype
 
 
-def mutation_event():
-    haplotype = get_random_haplotype()
+def mutation_event(pop):
+    haplotype = get_random_haplotype(pop)
     if pop[haplotype] > 1:
         pop[haplotype] -= 1
-        new_haplotype = get_mutant(haplotype)
+        new_haplotype = get_mutant(pop, haplotype)
         if new_haplotype in pop:
             pop[new_haplotype] += 1
         else:
             pop[new_haplotype] = 1
 
-def mutation_step():
-    mutation_count = get_mutation_count()
+def mutation_step(pop,mu):
+    mutation_count = get_mutation_count(pop,mu)
     for i in range(mutation_count):
-        mutation_event()
+        mutation_event(pop)
 
 
-def get_fitness(awm, amw, s, frequencies):
+def get_fitness(awm, amw, s, frequencies,pop, generations=100,
+ pop_size=sum(pop.values())):
     payoff=[[1, 1+awm], [1+s+amw, 1+s]]
     x = frequencies
     f = np.dot(payoff,x)
     fitness = {'0':f[0], '1':f[1]}
+    #print(f"fitness {pop}")
     return fitness
 
 
-def get_offspring_counts(s,x,y):
+def get_offspring_counts(s,x,y,pop):
+    pop_size=sum(pop.values())
     haplotypes = list(pop.keys())
     frequencies = [pop[haplotype]/float(pop_size) for haplotype in haplotypes]
-    fitness = get_fitness(x,y,s,frequencies)
+    fitness = get_fitness(x,y,s,frequencies,pop)
     fitnesses = [fitness[haplotype] for haplotype in haplotypes]
     weights = [x * y for x,y in zip(frequencies, fitnesses)]
     total = sum(weights)
     weights = [x / total for x in weights]
     #print(weights)
+    #print(f"offspring {pop} and {pop_size}")
     return list(np.random.multinomial(pop_size, weights))
 
 
-def offspring_step(s,x,y):
+def offspring_step(s,x,y,pop):
     haplotypes = list(pop.keys())
-    counts = get_offspring_counts(s,x,y)
+    counts = get_offspring_counts(s,x,y,pop)
+    #print(counts)
     for (haplotype, count) in zip(haplotypes, counts):
         if (count > 0):
             pop[haplotype] = count
         else:
             pop[haplotype] = 0
 
-def time_step(s,x,y):
-    mutation_step()
-    offspring_step(s,x,y)
+def time_step(s,x,y,pop, mu):
+    mutation_step(pop, mu)
+    offspring_step(s,x,y,pop)
 
 
-
-def simulate(s,x,y):
+def simulate(s,x,y,pop, mu, generations=100):
+    pop_size=sum(pop.values())
     history = []
     clone_pop = dict(pop)
     history.append(clone_pop)
     for i in range(generations):
-        time_step(s,x,y)
+        time_step(s,x,y,pop, mu)
         clone_pop = dict(pop)
         history.append(clone_pop)
     return(history)
